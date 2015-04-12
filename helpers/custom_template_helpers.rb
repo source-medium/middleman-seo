5732ff44078908052baeab322df1f0dc4df32713
Feifan Wang <feifanw@gmail.com>
2015-04-12 14:19:14 -0700

    template helper update: add social share url generator methods

--- helpers/custom_template_helpers.rb
+++ helpers/custom_template_helpers.rb
@@ -8,41 +8,49 @@ module CustomTemplateHelpers
     current_page.data.description || data.site.description
   end
 
-  def page_url
+  def current_page_url
     "#{data.site.url}#{current_page.url}"
+  end
+
+  def page_url page
+    "#{data.site.url}#{page.url}"
   end
 
   def page_twitter_card_type
     current_page.data.twitter_card_type || 'summary'
   end
 
+  def page_image
+    current_page.data.image_path || data.site.logo_image_path
+  end
+
   # Social share URLs
-  # def twitter_url
-  #   "https://twitter.com/share?text=“#{current_page.data.title}”" +
-  #                              "&url=#{current_article_url}" +
-  #                              "&via=SecretSauceHQ"
-  # end
-  #
-  # def facebook_url
-  #   "https://www.facebook.com/dialog/feed?app_id=952268334783316" +
-  #                                         "&caption=Secret Sauce Marketing Blog." +
-  #                                         "&picture=http://www.secretsaucehq.com#{image_path 'secret-sauce-logo.png'}" +
-  #                                         "&name=“#{current_article.title}”" +
-  #                                         "&description=Learn about all things digital marketing and SEO - how to use these channels to acquire customers and get traction for your business." +
-  #                                         "&display=popup" +
-  #                                         "&link=#{current_article_url}" +
-  #                                         "&redirect_uri=#{current_article_url}"
-  # end
-  #
-  # def google_plus_url
-  #   "https://plus.google.com/share?url=#{current_article_url}"
-  # end
-  #
-  # def linkedin_url
-  #   "http://www.linkedin.com/shareArticle?mini=true" +
-  #                                         "&url=#{current_article_url}" +
-  #                                         "&title=#{current_article.title}" +
-  #                                         "&summary=Learn about all things digital marketing and SEO - how to use these channels to acquire customers and get traction for your business." +
-  #                                         "&source=http://SecretSauceHQ.com/blog"
-  # end
+  def twitter_url
+    "https://twitter.com/share?text=“#{page_title}”" +
+                               "&url=#{current_page_url}" +
+                               "&via=#{data.site.twitter_handle}"
+  end
+
+  def facebook_url
+    "https://www.facebook.com/dialog/feed?app_id=#{data.site.facebook_app_id}" +
+                                          "&caption=#{page_title}" +
+                                          "&picture=#{page_image}" +
+                                          "&name=“#{page_title}”" +
+                                          "&description=#{page_description}" +
+                                          "&display=popup" +
+                                          "&link=#{current_page_url}" +
+                                          "&redirect_uri=#{current_page_url}"
+  end
+
+  def google_plus_url
+    "https://plus.google.com/share?url=#{current_page_url}"
+  end
+
+  def linkedin_url
+    "http://www.linkedin.com/shareArticle?mini=true" +
+                                          "&url=#{current_page_url}" +
+                                          "&title=#{page_title}" +
+                                          "&summary=#{page_description}" +
+                                          "&source=#{data.site.url}"
+  end
 end
